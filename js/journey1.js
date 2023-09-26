f886528b2b8489da736f8fd7cd890cd9a7ef4cd9
Lemenius <146039908+Lemenius@users.noreply.github.com>
2023-09-26 16:43:55 -0700

    Update journey1.js

--- js/journey1.js
+++ js/journey1.js
@@ -1,13 +1,15 @@
-function createStampedImage(src, top, left) {
-const stampedImage = document.createElement('img');
-stampedImage.src = src;
-stampedImage.alt = 'Stamped Image';
-stampedImage.classList.add('stamped-image');
-stampedImage.style.top = `${top}px`;
-stampedImage.style.left = `${left}px`;
-document.getElementById('imageContainer').appendChild(stampedImage);
-}
+document.addEventListener('DOMContentLoaded', () => {
+  function createStampedImage(src, top, left) {
+    const stampedImage = document.createElement('img');
+    stampedImage.src = src;
+    stampedImage.alt = 'Stamped Image';
+    stampedImage.classList.add('stamped-image');
+    stampedImage.style.top = `${top}px`;
+    stampedImage.style.left = `${left}px`;
+    document.getElementById('imageContainer').appendChild(stampedImage);
+  }
 
-createStampedImage('https://via.placeholder.com/300', 50, 50);
-createStampedImage('https://via.placeholder.com/300', 50, -500);
-createStampedImage('https://via.placeholder.com/300', 50, 500);
+  createStampedImage('https://via.placeholder.com/300', 50, 50);
+  createStampedImage('https://via.placeholder.com/300', 50, -500);
+  createStampedImage('https://via.placeholder.com/300', 50, 500);
+});
