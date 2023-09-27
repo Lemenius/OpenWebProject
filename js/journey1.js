bc4d06a38e834ab4f6f74404a4d30498b932c026
Lemenius <146039908+Lemenius@users.noreply.github.com>
2023-09-26 17:42:45 -0700

    Update journey1.js

--- js/journey1.js
+++ js/journey1.js
@@ -1,3 +1,4 @@
+document.addEventListener('DOMContentLoaded', () => {
 function createStampedImage(src, top, left, animationDelay) {
     const stampedImage = document.createElement('img');
     stampedImage.src = src;
@@ -6,23 +7,22 @@ function createStampedImage(src, top, left, animationDelay) {
     stampedImage.style.top = `${top}px`;
     stampedImage.style.left = `${left}px`;
     document.getElementById('imageContainer').appendChild(stampedImage);
-  }, animationDelay);
-}
+  }
 
-document.addEventListener('DOMContentLoaded', () => {
-  createStampedImage('https://imgpile.com/images/D9KINP.md.png', 50, 50, 1000);  // Add a 1-second delay for the first image
-  createStampedImage('https://imgpile.com/images/D9KINP.md.png', 50, -500, 2000);  // Add a 2-second delay for the second image
-  createStampedImage('https://imgpile.com/images/D9KINP.md.png', 50, 500, 3000);  // Add a 3-second delay for the third image
+  createStampedImage('https://imgpile.com/images/D9KINP.md.png', 50, 50, 1000); 
+  createStampedImage('https://imgpile.com/images/D9KINP.md.png', 50, -500, 2000); 
+  createStampedImage('https://imgpile.com/images/D9KINP.md.png', 50, 500, 3000); 
 
   function fadeInImage() {
     const fadeInImage = document.createElement('img');
-    fadeInImage.src = 'https://imgpile.com/images/DCfr9l.png';
+    fadeInImage.src = src;
     fadeInImage.alt = 'Fading In Image';
     fadeInImage.classList.add('fading-image');
-    fadeInImage.style.top = '150px';  // Adjust top position as needed
-    fadeInImage.style.left = '300px';  // Adjust left position as needed
+    fadeInImage.style.top = '150px';  
+    fadeInImage.style.left = '300px';  
     document.getElementById('imageContainer').appendChild(fadeInImage);
   }
 
-  setTimeout(fadeInImage, 4000);  // Adjust the delay as needed
-});
+    createFadeInImage('https://imgpile.com/images/DCfr9l.png', 50, 50, 1000);
+
+};
