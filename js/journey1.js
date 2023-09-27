cdabaca1f42f24f1d7cc8ee09b0629803ffecb9e
Lemenius <146039908+Lemenius@users.noreply.github.com>
2023-09-26 17:22:27 -0700

    Update journey1.js

--- js/journey1.js
+++ js/journey1.js
@@ -9,7 +9,16 @@ document.addEventListener('DOMContentLoaded', () => {
     document.getElementById('imageContainer').appendChild(stampedImage);
   }
 
+  function fadeInImage() {
+    const fadeInImage = document.createElement('img');
+    fadeInImage.src = 'https://imgpile.com/images/DCfr9l.png';
+    fadeInImage.alt = 'Fading In Image';
+    fadeInImage.classList.add('fading-image');
+    fadeInImage.style.top = '150px';
+    fadeInImage.style.left = '300px';
+    document.getElementById('imageContainer').appendChild(fadeInImage);
+  }
+
   createStampedImage('https://imgpile.com/images/D9KINP.md.png', 50, 50);
   createStampedImage('https://imgpile.com/images/D9KINP.md.png', 50, -500);
   createStampedImage('https://imgpile.com/images/D9KINP.md.png', 50, 500);
-});
