cea806f3c11478937772ceb1225cfbfbc5655602
Lemenius <146039908+Lemenius@users.noreply.github.com>
2023-09-27 23:37:56 -0700

    Update journey1.js

--- js/journey1.js
+++ js/journey1.js
@@ -6,51 +6,15 @@ tabContainers.forEach(tab => {
 
 function toggleTab(tabId) {
   const tab = document.getElementById(tabId);
-
-  // Fade out the current tab's content
   if (currentTab && currentTab !== tab) {
-    fadeOut(currentTab);
+    currentTab.style.display = 'none';
   }
-
   if (tab.style.display === 'none' || tab.style.display === '') {
-    // Fade in the tab's content
-    fadeIn(tab);
+    tab.style.display = 'block';
     currentTab = tab;
   } else {
-    // Fade out the tab's content
-    fadeOut(tab);
+    tab.style.display = 'none';
     currentTab = null;
   }
 }
 
-function fadeIn(element) {
-  element.style.opacity = 0;
-
-  const fadeInAnimation = element.animate(
-    [{ opacity: 0 }, { opacity: 1 }],
-    {
-      duration: 500, // Adjust the duration as needed
-      easing: 'ease-in',
-      fill: 'forwards'
-    }
-  );
-
-  fadeInAnimation.onfinish = () => {
-    element.style.opacity = 1;
-  };
-}
-
-function fadeOut(element) {
-  const fadeOutAnimation = element.animate(
-    [{ opacity: 1 }, { opacity: 0 }],
-    {
-      duration: 500, // Adjust the duration as needed
-      easing: 'ease-out',
-      fill: 'forwards'
-    }
-  );
-
-  fadeOutAnimation.onfinish = () => {
-    element.style.display = 'none';
-  };
-}
