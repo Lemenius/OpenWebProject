a14ab7bf9ef23217e87f2582025d14088187ae6f
Lemenius <146039908+Lemenius@users.noreply.github.com>
2023-09-27 17:13:29 -0700

    Update journey1.js

--- js/journey1.js
+++ js/journey1.js
@@ -1,12 +1,16 @@
 let currentTab = null;
+const tabContainers = document.querySelectorAll('.tab-container');
+tabContainers.forEach(tab => {
+  tab.style.display = 'none';
+});
 function toggleTab(tabId) {
   const tab = document.getElementById(tabId);
   if (currentTab && currentTab !== tab) {
-    currentTab.style.display = 'none'; 
+    currentTab.style.display = 'none';
   }
   if (tab.style.display === 'none' || tab.style.display === '') {
     tab.style.display = 'block';
-    currentTab = tab; 
+    currentTab = tab;
   } else {
     tab.style.display = 'none';
     currentTab = null;
